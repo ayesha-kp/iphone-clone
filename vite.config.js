4aeded703dc08e8d2c1041a9e63b9de496bbdad9
MEHVISH PATEL <mehvishpatel@outlook.com>
2024-12-13 23:30:38 +0530

    Update base path in vite.config.js for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : "/iphone-clone"
+   base: process.env.NODE_ENV === 'production' ? '/iphone-clone/' : '/'
 })
